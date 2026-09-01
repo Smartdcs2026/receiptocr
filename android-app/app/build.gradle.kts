448d58388cfb1a92772a73c4f969836eb93caa09
SKDC_SMART <somchaibutphon@gmail.com>
2026-09-02 02:05:52 +0700

    Round77 bump Android version to 0.77.0

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.receiptocr.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 76
-        versionName = "0.76.0"
+        versionCode = 77
+        versionName = "0.77.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
