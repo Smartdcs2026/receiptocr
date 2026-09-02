06aba8a05f23db5c3c71d85da3e304f68f36e9f1
SKDC_SMART <somchaibutphon@gmail.com>
2026-09-03 01:07:59 +0700

    Round91 bump APK version

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.receiptocr.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 92
-        versionName = "0.90.1"
+        versionCode = 93
+        versionName = "0.91.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
