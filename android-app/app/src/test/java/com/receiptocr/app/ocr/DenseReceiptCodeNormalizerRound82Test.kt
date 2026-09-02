7e605f0a694b41023618ee6c8c71c908f45b45f8
SKDC_SMART <somchaibutphon@gmail.com>
2026-09-02 13:20:29 +0700

    Round82 add real receipt code normalization tests

---
+++ android-app/app/src/test/java/com/receiptocr/app/ocr/DenseReceiptCodeNormalizerRound82Test.kt
@@ -0,0 +1,43 @@
+package com.receiptocr.app.ocr
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DenseReceiptCodeNormalizerRound82Test {
+
+    @Test
+    fun keepsMb02RealCodeUnchangedWhenAlreadyCorrect() {
+        assertEquals(
+            "R201051846U110030 20/08/69 17:51",
+            OcrTextNormalizer.normalizeLine("R201051846U110030 20/08/69 17:51")
+        )
+    }
+
+    @Test
+    fun restoresUWhenDotMatrixPrintIsReadAsVBetweenLongNumberGroups() {
+        assertEquals(
+            "R201051846U110030 20/08/69 17:51",
+            OcrTextNormalizer.normalizeLine("R201051846V110030 20/08/69 17:51")
+        )
+    }
+
+    @Test
+    fun restoresCommonDigitLikeCharactersOnlyInsideNumberRuns() {
+        assertEquals(
+            "R202039030U400072 20/08/69 17:18",
+            OcrTextNormalizer.normalizeLine("R20203903OU400O72 20/08/69 17:18")
+        )
+        assertEquals(
+            "R202039530U400072 20/08/69 17:18",
+            OcrTextNormalizer.normalizeLine("R202039S30U400072 20/08/69 17:18")
+        )
+    }
+
+    @Test
+    fun doesNotChangeOrdinaryWordsContainingV() {
+        assertEquals(
+            "VAT INCLUDED",
+            OcrTextNormalizer.normalizeLine("VAT INCLUDED")
+        )
+    }
+}
