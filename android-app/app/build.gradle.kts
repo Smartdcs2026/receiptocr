e02f686ea30d65873bec59e70da224b69b444b13
SKDC_SMART <somchaibutphon@gmail.com>
2026-09-06 08:14:45 +0700

    Round104 hotfix bump evidence finalize build

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.receiptocr.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 106
-        versionName = "0.104.0"
+        versionCode = 107
+        versionName = "0.104.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
