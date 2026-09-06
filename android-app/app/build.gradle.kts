d461d307ff1b61f876ff8a3a8187fbb9ec6f0617
SKDC_SMART <somchaibutphon@gmail.com>
2026-09-06 23:24:14 +0700

    Round104.2 bump Android version for direct evidence upload

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.receiptocr.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 107
-        versionName = "0.104.1"
+        versionCode = 108
+        versionName = "0.104.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
