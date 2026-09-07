ac2c13cefaa2789a22ac86158764fe19560188ec
SKDC_SMART <somchaibutphon@gmail.com>
2026-09-08 01:41:49 +0700

    Round104.17 bump Android for terminal POS rules

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.receiptocr.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 108
-        versionName = "0.104.2"
+        versionCode = 109
+        versionName = "0.104.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
